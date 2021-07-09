06a92c3234864ae9cea55e6b41a098cb1b98e9c4
demokan01 <dmkn01@hotmail.com>
2021-07-09 23:09:50 +0300

    Generic Repository Oluşturuldu

--- API.Infrastructure/Implements/ProductRepository.cs
+++ API.Infrastructure/Implements/ProductRepository.cs
@@ -16,7 +16,10 @@ namespace API.Infrastructure.Implements
         }
         public async Task<IReadOnlyList<Product>> GetProductAsync()
         {
-            return await _context.Products.ToListAsync();
+            return await _context.Products
+                .Include(p=>p.ProductBrand)
+                .Include(p=>p.ProductType)
+                .ToListAsync();
         }
 
         public async Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync()
@@ -31,7 +34,10 @@ namespace API.Infrastructure.Implements
         /// <returns></returns>
         public async Task<Product> GetProductByIdAsync(int id)
         {
-            return await _context.Products.FindAsync(id);
+            return await _context.Products
+                .Include(p => p.ProductBrand)
+                .Include(P => P.ProductType)
+                .FirstOrDefaultAsync(p => p.Id == id);
             
         }
 
